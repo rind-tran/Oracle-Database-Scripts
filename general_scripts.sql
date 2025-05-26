eb095f6d5cfc88ef5c6563f2a8f432e799f49572
rind-tran <53333438+rind-tran@users.noreply.github.com>
2025-05-26 10:34:25 +0700

    Update general_scripts.sql

--- general_scripts.sql
+++ general_scripts.sql
@@ -433,3 +433,123 @@ SELECT DISTINCT a.sql_id,
        AND a.user_id <> 0                                  -- exclude SYS user
        AND a.sample_time BETWEEN SYSDATE - 3 AND SYSDATE - 2
        AND a.current_obj# = o.object_id
+
+-- 37 Archive log transfer rate
+SELECT LOG_HISTORY.*,
+         SUM_ARCH.GENERATED_MB,
+         SUM_ARCH_DEL.DELETED_MB,
+         SUM_ARCH.GENERATED_MB - SUM_ARCH_DEL.DELETED_MB "REMAINING_MB"
+    FROM (  SELECT TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY') DAY,
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '00', 1, NULL))
+                      "00-01",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '01', 1, NULL))
+                      "01-02",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '02', 1, NULL))
+                      "02-03",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '03', 1, NULL))
+                      "03-04",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '04', 1, NULL))
+                      "04-05",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '05', 1, NULL))
+                      "05-06",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '06', 1, NULL))
+                      "06-07",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '07', 1, NULL))
+                      "07-08",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '08', 1, NULL))
+                      "08-09",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '09', 1, NULL))
+                      "09-10",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '10', 1, NULL))
+                      "10-11",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '11', 1, NULL))
+                      "11-12",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '12', 1, NULL))
+                      "12-13",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '13', 1, NULL))
+                      "13-14",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '14', 1, NULL))
+                      "14-15",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '15', 1, NULL))
+                      "15-16",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '16', 1, NULL))
+                      "16-17",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '17', 1, NULL))
+                      "17-18",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '18', 1, NULL))
+                      "18-19",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '19', 1, NULL))
+                      "19-20",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '20', 1, NULL))
+                      "20-21",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '21', 1, NULL))
+                      "21-22",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '22', 1, NULL))
+                      "22-23",
+                   SUM (
+                      DECODE (TO_CHAR (COMPLETION_TIME, 'HH24'), '23', 1, NULL))
+                      "23-00",
+                   COUNT (*) TOTAL
+              FROM V$ARCHIVED_LOG
+             WHERE ARCHIVED = 'YES'
+          GROUP BY TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY')) LOG_HISTORY,
+         (  SELECT TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY') DAY,
+                   SUM (ROUND ( (blocks * block_size) / (1024 * 1024), 2))
+                      GENERATED_MB
+              FROM V$ARCHIVED_LOG
+             WHERE ARCHIVED = 'YES'
+          GROUP BY TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY')) SUM_ARCH,
+         (  SELECT TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY') DAY,
+                   SUM (ROUND ( (blocks * block_size) / (1024 * 1024), 2))
+                      DELETED_MB
+              FROM V$ARCHIVED_LOG
+             WHERE ARCHIVED = 'YES' AND DELETED = 'YES'
+          GROUP BY TO_CHAR (COMPLETION_TIME, 'DD/MM/YYYY')) SUM_ARCH_DEL
+   WHERE LOG_HISTORY.DAY = SUM_ARCH.DAY AND SUM_ARCH.DAY = SUM_ARCH_DEL.DAY(+)
+ORDER BY TO_DATE (LOG_HISTORY.DAY, 'DD/MM/YYYY');
+
+-- 38 Historical CPU and memory usage by service (CDB level only)
+select * 
+from DBA_HIST_SYSMETRIC_SUMMARY
+where snap_id=7512
+and metric_name in ('Host CPU Utilization (%)','I/O Megabytes per Second','I/O Requests per Second','Total PGA Allocated')
+
+-- 39 Find SQL text base on SID
+select
+a.sid,
+a.serial#,
+b.sql_text
+from
+v$session a,
+v$sqlarea b
+where
+a.sql_address=b.address
+and
+sid=3696;
+
+-- 40 Kill session
+select   'alter system kill session '||''''|| a.SID ||','|| a.SERIAL# ||',@'|| a.inst_id || ''''||' immediate;'
+ from gv$session a where a.USERNAME='DCAL';
